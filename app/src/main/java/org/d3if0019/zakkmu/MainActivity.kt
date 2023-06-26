30845397e3f508c5de18bea58e2025ed4c7b86c3
denna auliya putri ambya <dennaputri@student.telkomuniversity.ac.id>
2023-06-26 14:04:51 +0700

    menambah view binding

--- app/src/main/java/org/d3if0019/zakkmu/MainActivity.kt
+++ app/src/main/java/org/d3if0019/zakkmu/MainActivity.kt
@@ -2,10 +2,20 @@ package org.d3if0019.zakkmu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if0019.zakkmu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener { hitungZakat() }
+
+    }
+    private fun hitungZakat(){
+        Log.d("MainActivity","Tombol diklik!")
     }
 }
